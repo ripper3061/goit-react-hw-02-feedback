2e14642a629ed293448ef63d256be9bf56eb9c97
Vladimir Ponomarov <ripper3061@gmail.com>
2023-03-18 12:23:13 +0200

    step 2

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,17 +1,72 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export const Feedback = () => {
-  return (
-    <div>
-      <h2>Please leave feedback</h2>
-      <button type="button">Good</button>
-      <button type="button">Neutral</button>
-      <button type="button">Bad</button>
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-      <h2>Statistic</h2>
-      <p>Good:</p>
-      <p>Neutral:</p>
-      <p>Bad:</p>
-    </div>
-  );
-};
+  clickOnGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+
+  clickOnNeutral = () => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+
+  clickOnBad = () => {
+    this.setState(prevState => {
+      return { bad: prevState.bad + 1 };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round(
+      ((this.countTotalFeedback() - this.state.neutral - this.state.bad) *
+        100) /
+        this.countTotalFeedback()
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <button type="button" onClick={this.clickOnGood}>
+          Good
+        </button>
+        <button type="button" onClick={this.clickOnNeutral}>
+          Neutral
+        </button>
+        <button type="button" onClick={this.clickOnBad}>
+          Bad
+        </button>
+
+        <h2>Statistic</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+        <p>Total: {this.countTotalFeedback()}</p>
+        <p>
+          Positive feedback:
+          {this.countPositiveFeedbackPercentage() === NaN
+            ? 0
+            : this.countPositiveFeedbackPercentage()}
+          %
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
